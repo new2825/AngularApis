42af2becc772aa7b4f7f82e9a40e21d38b2b41fd
Oscar Herrán <new282566@gmail.com>
2023-12-19 16:52:25 +0100

    Base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularApis';
+  title = 'MyJob';
+  items: any[] = [
+    { label: 'inicio', icon: 'pi pi-home'},
+    { label: 'quienes somon', icon: 'pi pi-home'},
+    { label: 'contacto', icon: 'pi pi-home'},
+    { label: 'login', icon: 'pi pi-home'},
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http'
+
+/* primeng components */
+import { MenuModule } from 'primeng/menu';
+import { BadgeModule } from 'primeng/badge';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MenuModule,
+    BadgeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
